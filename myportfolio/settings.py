ce6048d56be17ad002073dc316442df602b12e2c
alefra88 <efra.rlz@gmail.com>
2022-05-13 01:27:40 -0500

    cosillas

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -127,5 +127,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'myportfolio/static/'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static/'),)
 STATIC_URL='/static/'
