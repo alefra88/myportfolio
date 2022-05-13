0e3bbf230957b160759a67426388a923e9fb54d0
alefra88 <efra.rlz@gmail.com>
2022-05-13 01:45:08 -0500

    cambiosss

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,13 +124,17 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
+MEDIA_URL = 
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static/'),)
+STATICFILES_DIRS = (
+    (os.path.join(BASE_DIR,'smcore','static')),
+)
 STATIC_URL='/static/'
